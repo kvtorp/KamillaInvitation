a567e9cd054c983ee68816e9e510a880b3ce8668
Kasper Vilhelm Torp <kvtorp@gmail.com>
2019-08-07 10:09:41 +0200

    Initial commit

---
+++ src/sql/create.sql
@@ -0,0 +1,5 @@
+use id1537357_kamilla;
+
+drop table if exists bruger;
+
+CREATE TABLE `id1537357_kamilla`.`bruger` ( `hemmelighed` INT NOT NULL , `navn` TEXT NOT NULL , PRIMARY KEY (`hemmelighed`)) ENGINE = InnoDB;

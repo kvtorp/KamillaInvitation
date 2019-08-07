6a862d96e4daba1e3370de9c5827ab037264909b
Kasper Vilhelm Torp <kvtorp@gmail.com>
2019-08-08 01:03:37 +0200

    demo

--- src/sql/create.sql
+++ src/sql/create.sql
@@ -1,5 +1,8 @@
 use id1537357_kamilla;
 
 drop table if exists bruger;
+drop table if exists medbragt;
 
 CREATE TABLE `id1537357_kamilla`.`bruger` ( `hemmelighed` INT NOT NULL , `navn` TEXT NOT NULL , PRIMARY KEY (`hemmelighed`)) ENGINE = InnoDB;
+CREATE TABLE `id1537357_kamilla`.`medbragt` ( `medbragtid` INT NOT NULL , `medbragt` TEXT NOT NULL , `detaljer` TEXT NOT NULL , `hemmelighed` INT NOT NULL , PRIMARY KEY (`medbragtid`)) ENGINE = InnoDB;
+
